5c67b0660a7d319991d56dee4daf7be104ef040a
Harrison Malone <harrison.malone2@gmail.com>
2020-11-24 23:25:08 +1100

    added favicon and made pwa

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,39 @@
 import { useState, useEffect } from "react";
 import showdown from "showdown";
+import moment from "moment";
 
 const Post = (props) => {
-  const [post, setPost] = useState(null)
+  const [post, setPost] = useState(null);
+  const [html, setHtml] = useState("");
   const id = props.match.params.id;
 
   useEffect(() => {
     const fetchPost = async () => {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/posts/${id}`);
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/posts/${id}`
+      );
       const post = await response.json();
       const converter = new showdown.Converter();
       const html = converter.makeHtml(post.body);
-      setPost(html)
-    }
-    fetchPost()
-  }, [id])
+      setHtml(html);
+      setPost(post);
+    };
+    fetchPost();
+  }, [id]);
 
   const createMarkup = () => {
-    return { __html: post };
+    return { __html: html };
   };
 
-  return post && <div dangerouslySetInnerHTML={createMarkup()}></div>;
-}
+  console.log(post);
+  return (
+    post && (
+      <div>
+        <span style={{fontSize: "large", color:"#717369"}}>{moment(post.created_at).format("MMMM Do YYYY, h:mm:ss a")}</span>
+        <div dangerouslySetInnerHTML={createMarkup()}></div>
+      </div>
+    )
+  );
+};
 
 export default Post;
