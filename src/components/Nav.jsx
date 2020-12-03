26fc29148ec69ab16940fabb0e28d91a429c76a7
Harrison Malone <harrison.malone2@gmail.com>
2020-12-03 15:14:12 +1100

    reload icon added to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,10 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { PostsContext } from "../context/PostsContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSync
+} from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
   const { auth } = useContext(AuthContext);
@@ -15,6 +19,7 @@ const Nav = () => {
       <div className="nav-action-links">
         {auth && (
           <>
+            <FontAwesomeIcon icon={faSync} style={{fontSize: "12px", cursor: "pointer"}} onClick={() => window.location.reload()} />
             <Link to="/">Write</Link>
             <Link to="/preview">Preview</Link>
           </>
